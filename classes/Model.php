62fe88f9047d9b86efd1a3746cc9818335d4a814
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2014-12-21 14:43:01 +0100

    Added modal file

---
+++ classes/Model.php
@@ -0,0 +1,45 @@
+<?php
+namespace lifelinearts;
+
+class Model
+{
+	private static $instance;
+
+	private function __construct(){
+
+	}
+
+	public static function getInstance(){
+		$ret = '';
+		if(isset(self::$instance)){
+			$ret = self::$instance;
+		}else{
+			self::$instance = new Model();
+			$ret = self::$instance;
+			
+		}
+		return $ret;
+	}
+
+	public function add($array){
+		foreach ($array as $index => $values) {
+          if (is_array($values) && empty($values)) {
+            	$this->{$index} = new stdClass();
+          	} else if (is_array($values)) {
+               $this->{$index} = $this->add($values);
+           	} else {
+               $this->{$index} = $values;
+           }
+         }
+
+	}
+
+	public function set( $class){
+
+		self::$instance = $class;
+	}
+
+	
+}
+
+?>

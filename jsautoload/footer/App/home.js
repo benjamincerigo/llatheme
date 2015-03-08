d16c6c1c730990c6dc2b262e29534e4ca63132e9
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2015-03-08 16:19:50 +0100

    Object functionallity working with calender

--- jsautoload/footer/App/home.js
+++ jsautoload/footer/App/home.js
@@ -28,12 +28,19 @@ window.angular.module('llaapp.home', [	'llaapp.inlineservices'])
 
 	};
 	this.initAbout = function(){
+		var a = this.getSection('about'),
+				s = this.lla_search(a.content, 'main');
+		if(s === false){
+			a.content.main = {};
+		}
+		a.content.main.selectedbool = false;
 	};
 	this.initCalender = function(){
 		console.log(this.model.content.Calender);
 		var m = this.model.content.Calender,
 			k = Object.keys(m.content.events)[0] ,
 			i = m.content.events[k];
+		this.deselectAll(m.content.events);
 		m.content.main = i;
 		i.selectedbool = true;
 	};
@@ -58,16 +65,79 @@ window.angular.module('llaapp.home', [	'llaapp.inlineservices'])
 		}
 		return r;
 	};
+	this.lla_search = function(o, find){
+		if(o.hasOwnProperty(find)){
+			return o[find];	
+		}else{
+			return false;
+		}
+	};
 	this.processState = function($stateParams){
 		console.log('called function');
+		var s = this.lla_search($stateParams, 'section'),
+			p = this.lla_search($stateParams, 'part');
 
-		console.log($stateParams);
+		if( p === false){
+			return null;
+		}
+		switch(s){
+			case 'about':
+				this.aboutState(this.getSection('about'), p);
+				break;
+			case 'calender':
+				this.calenderState(this.getSection('calender'), p);
+				break;
+			default:
+				return null;
+		}
+	};
+	this.aboutState = function(a,p){
+		var s;
+		if( p === '~' ){
+			this.initAbout();
+		}else{
+			s = this.lla_search(a.content.posts, p);
+			if(s === false){
+				return null;
+			}
+			console.log(s);
+			a.content.main = s;
+			a.content.main.selectedbool = true;
+		}
+	};
+	this.calenderState = function(c,p){
+		var s;
+		if( p === '~' ){
+			this.initCalender();
+		}else{
+			s = this.lla_search(c.content.events, p);
+			console.log(s);
+			if(s === false){
+				return null;
+			}
+			c.content.main.selectedbool = false;
+			this.deselectAll(c.content.events);
+			c.content.main.selectedbool = true;
+			c.content.main = s;
+		}
+	};
+	this.deselectAll = function(ob){
+		Object.keys(ob).forEach(function(key ) {
+			this[key].selectedbool = false;
+		}, ob);
 	};
 	this.$get = function (){
 		var r = {
 			model: this.model,
 			getSection: this.getSection,
-			state: this.processState
+			state: this.processState,
+			lla_search: this.lla_search,
+			initAbout: this.initAbout,
+			aboutState: this.aboutState,
+			initCalender: this.initCalender,
+			calenderState: this.calenderState,
+				//Utils
+			deselectAll: this.deselectAll,
 		};
 		return r;
 	};
